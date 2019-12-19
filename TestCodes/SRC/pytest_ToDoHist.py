3850a0046b60f0c2b0d547acf70b0203e099e6f6
hsiaojietng <s10177814@connect.np.edu.sg>
2019-12-19 12:11:21 +0800

    Failed Display archived items test case

---
+++ TestCodes/SRC/pytest_ToDoHist.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import pytest
+
+def DisplayListofArchivedItems():
+    browser.get("http://127.0.0.1:8000/todohist/")
+    #returns the name in ul tag which is "List of Archived Items"
+    return browser.find_element_by_xpath("//ul").get_attribute("name")
+    
+
+browser = webdriver.Chrome()
+DisplayListofArchivedItems()
+
---
+++ TestCodes/test_cases_ToDoHist.py
@@ -0,0 +1,6 @@
+import pytest
+from SRC.pytest_Nav&Login import *
+
+#Testing Navigation and Login
+def test_DisplayListofArchivedItems():
+    assert "List of Archived Items"
