b98f3d62a2fa12e5d3754e6c91e6b054a8f0db9d
desireedeleon <desiree.deleon@elsanow.io>
2019-12-26 20:40:09 +0800

    passed add valid item to todo list test case

--- TestCodes/test_cases_ToDo.py
+++ TestCodes/test_cases_ToDo.py
@@ -2,10 +2,21 @@ import pytest
 from SRC.pytest_ToDo import *
 import re
 
-#Test Cases
-def test_DisplayListOfItems():
-    assert "List of Items"
+# Functions
+def addItems(string):
+    input = browser.find_element_by_name("content")
+    input.send_keys(string)
+    input.send_keys(Keys.RETURN)
+    return True
 
-def test_AddBlankItem():
-    browser.find_element_by_xpath("//input[@value='Add']").click()
-    assert "error" in browser.page_source
+#Test Cases
+# def test_DisplayListOfItems():
+#     assert "List of Items"
+#
+# def test_AddBlankItem():
+#     browser.find_element_by_xpath("//input[@value='Add']").click()
+#     assert "error" in browser.page_source
+
+def test_AddValidItem():
+    addItems("jd's test item")
+    assert "jd's test item" in browser.page_source
