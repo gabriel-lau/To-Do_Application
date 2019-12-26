83dbee38d5939b990ffcf7ec9186617be34056a6
desireedeleon <desiree.deleon@elsanow.io>
2019-12-26 20:41:47 +0800

    passed add existing item to todo list test case

--- TestCodes/test_cases_ToDo.py
+++ TestCodes/test_cases_ToDo.py
@@ -10,13 +10,22 @@ def addItems(string):
     return True
 
 #Test Cases
-# def test_DisplayListOfItems():
-#     assert "List of Items"
-#
-# def test_AddBlankItem():
-#     browser.find_element_by_xpath("//input[@value='Add']").click()
-#     assert "error" in browser.page_source
+def test_DisplayListOfItems():
+    assert "List of Items"
+
+def test_AddBlankItem():
+    browser.find_element_by_xpath("//input[@value='Add']").click()
+    assert "error" in browser.page_source
 
 def test_AddValidItem():
     addItems("jd's test item")
     assert "jd's test item" in browser.page_source
+
+def test_AddExistingItem():
+    addItems("jd's test item")
+    page = browser.page_source
+    count = 0
+    text = re.findall("jd's test item", page)
+    for i in text:
+        count+=1
+    assert count == 2
