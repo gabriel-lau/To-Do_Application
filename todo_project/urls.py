5375c314798b6473573ed4d0f898c3d394ef3d51
Ning <wanninglim@gmail.com>
2019-12-27 01:47:13 +0800

    Comment for add dummy user account

--- todo_project/urls.py
+++ todo_project/urls.py
@@ -34,11 +34,11 @@ urlpatterns = [
 ]
 
 # Creating a test user account
-# Create user and save to the database
-User.objects.filter(username = 'myusername').delete()
-user = User.objects.create_user('myusername', 'myemail@gmail.com', 'mypassword')
+# # Create user and save to the database
+# User.objects.filter(username = 'myusername').delete()
+# user = User.objects.create_user('myusername', 'myemail@gmail.com', 'mypassword')
 
-# Update fields and then save again
-user.first_name = 'John'
-user.last_name = 'Lim'
-user.save()
+# # Update fields and then save again
+# user.first_name = 'John'
+# user.last_name = 'Lim'
+# user.save()
