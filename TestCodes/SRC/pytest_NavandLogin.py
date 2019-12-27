d03e5ef108fb5736ff27edc7fb4c4873dae1a332
desireedeleon <desiree.deleon@elsanow.io>
2019-12-28 01:34:00 +0800

    Passed successful login test case

--- TestCodes/SRC/pytest_NavandLogin.py
+++ TestCodes/SRC/pytest_NavandLogin.py
@@ -3,6 +3,12 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import pytest
 
+def login(username, password):
+    browser.get("http://127.0.0.1:8000/todo")
+    user = browser.find_element_by_name("username").send_keys(username)
+    passwrd = browser.find_element_by_name("password").send_keys(password)
+    browser.find_element_by_xpath("//input[@value='login']").click()
+
 def NavigateToDoPage():
     browser.get("http://127.0.0.1:8000/todo/")
     #returns the heading To-Do List
@@ -13,13 +19,13 @@ def NavigateToDoHistory():
     #returns the heading To-Do History
     return browser.find_element_by_xpath("//h1")
 
-    
+
 
 
 
 
 browser = webdriver.Chrome()
+login("myusername", "mypassword")
 NavigateToDoPage()
 NavigateToDoHistory()
 browser.close()
-
