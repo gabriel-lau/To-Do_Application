7b06319f3441fd1f28dc9dd773d69fd103b985ad
desireedeleon <desiree.deleon@elsanow.io>
2019-12-28 01:39:57 +0800

    Passed failing login test case

--- TestCodes/test_cases_NavandLogin.py
+++ TestCodes/test_cases_NavandLogin.py
@@ -15,9 +15,16 @@ def test_Logout():
     browser.get("http://127.0.0.1:8000/accounts/logout/")
     assert "Logged out" in browser.page_source
 
-# def test_InvalidLogin():
-#     assert ""
-#
+@pytest.mark.parametrize("a,b",[
+("myusername", "wrongpassword"),
+("wrongusername", "wrongpassword"),
+]
+)
+def test_InvalidLogin(a,b):
+    browser.get("http://127.0.0.1:8000/accounts/login/")
+    login(a,b)
+    assert "Incorrect username and password. Please try again."
+
 # def test_CannotNavigateToDoPage():
 #     assert ""
 #
