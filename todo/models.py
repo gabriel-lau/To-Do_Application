31ff837af0918000141d46423196d986d2252975
YUNT3NG <yunt3ng@atlassian.com>
2019-12-29 21:21:59 +0800

    Implemented timestamp

--- todo/models.py
+++ todo/models.py
@@ -1,12 +1,13 @@
 from django.db import models
+from django.db.models import Model
 
 class ToDoItem(models.Model):
     content = models.TextField()
-    '''created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         get_latest_by = 'created_at'
-        '''
+        
 class ToDoHistory(models.Model):
     content = models.TextField()
