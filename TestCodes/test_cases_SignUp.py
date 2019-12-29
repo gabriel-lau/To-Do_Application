048891ea67ed7c9104b0bd480c6ab29cee7b9f7d
desireedeleon <desiree.deleon@elsanow.io>
2019-12-29 22:23:07 +0800

    Passed sign up with valid credentials test case

---
+++ TestCodes/test_cases_SignUp.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import pytest
+
+browser = webdriver.Chrome()
+
+def signup(username, password1, password2):
+    browser.get("http://127.0.0.1:8000/accounts/signup")
+    browser.find_element_by_name("username").send_keys(username)
+    browser.find_element_by_name("password1").send_keys(password1)
+    browser.find_element_by_name("password2").send_keys(password2)
+    browser.find_element_by_xpath("//input[@value='Sign Up']").click()
+
+def test_SignUpWithValidCredentials():
+    signup("janinedesiree", "ephemeral12", "ephemeral12")
+    assert "Please login to see this page." in browser.page_source
